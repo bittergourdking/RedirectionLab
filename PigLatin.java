2db9b32507817c1a14f8d78e1f9866d842f2aef5
bittergourdking <>
2021-01-05 21:27:27 -0500

    got everything working, tested with expectedoutput

--- PigLatin.java
+++ PigLatin.java
@@ -1,38 +1,59 @@
+import java.util.*;
 public class PigLatin {
   public static String pigLatinSimple(String s) {
-    String answer = "";
-    s.toLowerCase();
-    if (s.charAt(0) == 'a' || s.charAt(0) == 'e'
-    || s.charAt(0) == 'i' || s.charAt(0) == 'o' || s.charAt(0) == 'u') {
-      answer = s + "hay";
+    String lowerS = s.toLowerCase();
+    if (lowerS.charAt(0) == 'a' || lowerS.charAt(0) == 'e'
+    || lowerS.charAt(0) == 'i' || lowerS.charAt(0) == 'o' || lowerS.charAt(0) == 'u') {
+      return lowerS + "hay";
     } else {
-      answer = s.substring(1) + s.substring(0, 1) + "ay";
+      return lowerS.substring(1) + lowerS.substring(0, 1) + "ay";
     }
-    return answer;
   }
 
   public static String pigLatin(String s) {
-    s.toLowerCase();
+    String lowerS = s.toLowerCase();
     String[] digraph = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl",
     "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh",
     "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
-    if (s.length() > 1) {
+    if (lowerS.length() > 1) {
       for (int i = 0; i < digraph.length; i++) {
-        if (s.substring(0, 2).equals(digraph[i])) {
-          return s.substring(2) + s.substring(0, 2) + "ay";
+        if (lowerS.substring(0, 2).equals(digraph[i])) {
+          return lowerS.substring(2) + lowerS.substring(0, 2) + "ay";
         }
       }
     }
-    return pigLatinSimple(s);
+    return pigLatinSimple(lowerS);
   }
 
   public static String pigLatinBest(String s) {
-    String answer = "";
-    s.toLowerCase();
-    return answer;
+    String lowerS = s.toLowerCase();
+    if (Character.isLetter(lowerS.charAt(0))) {
+      if (Character.isLetter(lowerS.charAt(lowerS.length() - 1))
+      || Character.isDigit(lowerS.charAt(lowerS.length() - 1))) {
+        return pigLatin(lowerS);
+      } else {
+        String punc = lowerS.substring(lowerS.length() - 1);
+        s = pigLatin(lowerS.substring(0, lowerS.length() - 1));
+        return s + punc;
+      }
+    } else {
+      return lowerS;
+    }
   }
 
   public static void main(String[]args) {
-
+    Scanner n = new Scanner( System.in );
+    while (n.hasNextLine()) {
+      Scanner o = new Scanner ( n.nextLine() );
+      while (o.hasNext()) {
+        System.out.print(pigLatinBest(o.next()));
+        if (o.hasNext()) {
+          System.out.print(" ");
+        }
+      }
+      if (n.hasNextLine()) {
+        System.out.println();
+      }
+    }
   }
 }
